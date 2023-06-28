3d962f88f7bbd36455fbc4149455fcd55717b9d7
xMrAfonso <afonso.lu@hotmail.com>
2023-06-28 19:28:02 +0200

    Initial commit

---
+++ src/main/java/me/mrafonso/fundamentals/Fundamentals.java
@@ -0,0 +1,17 @@
+package me.mrafonso.fundamentals;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Fundamentals extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
